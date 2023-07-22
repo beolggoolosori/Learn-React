189f24e00607bfca0230001d7fcaf2e83e6240e2
Bellhoon-__- <ilbora@naver.com>
2023-07-23 00:49:21 +0900

    📝 docs: 4.props

---
+++ 4. Props/my-app/src/App.js
@@ -0,0 +1,37 @@
+import logo from './logo.svg';
+import './App.css';
+
+function Header(){
+  return <header>
+      <h1><a href="/">WEB</a></h1>
+    </header>
+}
+
+function Nav(){
+  return <nav>
+    <ol>
+      <li><a href="1.html">html</a></li>
+      <li><a href="2.html">css</a></li>
+      <li><a href="3.html">js</a></li>
+    </ol>  
+  </nav>
+}
+
+function Article(){
+  return <article>
+    <h2>Welcome</h2>
+    Hello, React
+  </article>
+}
+
+function App() {
+  return (
+    <div>
+      <Header></Header>
+      <Nav></Nav>
+      <Article></Article>
+    </div>
+  )
+}
+
+export default App;
